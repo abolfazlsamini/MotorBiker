1bb091cdba0aaac548116dec40a78c0c825cb666
abolfazl <email@abolfazls.ir>
2024-04-26 18:22:20 +0330

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
